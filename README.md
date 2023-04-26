1e88adc18d522b5924728e8d78f496d861521e16
Lewey <dlmelchor12@gmail.com>
2023-04-26 16:04:33 -0700

    rebuilt game added read me

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MagicEightBall
- 
+
+## Description
+a very simple terminal game that emulates the Magic Eight Ball from your childhood
+
+## Project Goal
+my main goal was to understand how to utilize other python packages to build a useable program
+
+## Start it up
+- open terminal at directory
+- start virtual enviornment
+    - source .venv/bin/activate
+- start game
+    - python eight-ball.py
+- Ask a question
+
+## Created By:
+
+|Name|Email|GitHub|
+|----|-----|-------|
+|David "Lewey" Melchor|dlmelchor12@gmail.com|https://github.com/leweymelchor|
--- eight-ball.py
+++ eight-ball.py
@@ -1,18 +1,6 @@
-is_workday = True
-is_sunny = False
+# Magic Eight Ball
+import eight_ball
 
-def gear_for_day(is_workday, is_sunny):
-    needs = []
-
-    if is_workday:
-        needs.append("laptop")
-    elif is_workday is False and is_sunny is False:
-        needs.append("umbrella")
-    elif is_workday is False and is_sunny:
-        needs.append("surfboard")
-    else:
-        pass
-
-    return needs
-
-print(gear_for_day(is_workday, is_sunny))
+ball = eight_ball.ball()
+response = ball.response(input("What do you want to know? "))
+print(response)
